47a87f82172748cdaada3ce885490048ef852bfe
yus <ajtdmwgp42431@gmail.com>
2021-12-19 20:26:14 +0900

    フォーム更新

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,7 @@ const Contact = () => (
     <div>
       <div>
         <h1>Contact</h1>
-        <form method="post" name="contact" data-netlify="true">
+        <form method="post" name="contact" netlify-honeypot="bot-field" data-netlify="true" >
           <input type="hidden" name="form-name" value="contact" />
           <label htmlFor="name">お名前</label>
           <input type="text" name="name" id="name" required />
