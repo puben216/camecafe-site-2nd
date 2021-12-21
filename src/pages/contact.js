84be7fd6d118336147eb4658a37131dce1a8d00c
yus <ajtdmwgp42431@gmail.com>
2021-12-21 21:06:19 +0900

    form 要素追加

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,10 +13,31 @@ const Contact = () => (
         <h1>Contact</h1>
         <form method="post" name="contact" netlify-honeypot="bot-field" data-netlify="true" >
           <input type="hidden" name="form-name" value="contact" />
-          <label htmlFor="name">お名前</label>
-          <input type="text" name="name" id="name" required />
-          <label></label>
-          <label>内容</label>
+          <div>
+            <label htmlFor="name">お名前：</label>
+            <input type="text" name="name" id="name" required /><br />
+          </div>
+          <div>
+            <label htmlFor="name">性別：</label>
+            <label htmlFor="men"><input type="radio" name="sex" value="men" />男</label>
+            <label htmlFor="women"><input type="radio" name="sex" value="woman" />女</label><br />
+          </div>
+          <div>
+            <label htmlFor="name">カメラ歴：</label>
+            <input type="text" name="experience" id="experience" required /><br />
+          </div>
+          <div>
+            <label htmlFor="name">メールアドレス：</label>
+            <input type="text" name="email" id="email" required /><br />
+          </div>
+          <div>
+            <label htmlFor="name">内容：</label>
+            <textarea name="content" id="" cols="30" rows="10">
+              撮影で行ってみたい所や、
+              応募理由等をご記入ください。
+            </textarea>
+          </div>
+
           <button type="submit">送信</button>
         </form>
       </div>
