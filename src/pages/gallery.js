6857517935c2ffce743997c3cde31436be186386
yus <ajtdmwgp42431@gmail.com>
2022-11-10 09:13:30 +0900

    ギャラリー修正

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,42 +1,56 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import Img from "gatsby-image"
 
 import Layout from "../components/layout"
-import Seo from "../components/seo" 
+import Seo from "../components/seo"
 import { graphql } from "gatsby"
-  
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUtensils, faCheckSquare } from "@fortawesome/free-solid-svg-icons"
 
-import StackGrid from "react-stack-grid"
-import { GatsbyImage, getImage } from "gatsby-plugin-image";
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { StaticImage } from "gatsby-plugin-image"
-
-
-
+import { Masonry } from "@mui/lab"
+import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material"
 
 export default ({ data, location }) => {
-  const GatsbyImages = data.about.edges.map(({node}) => {
+  const GatsbyImages = data.about.edges.map(({ node }) => {
     return (
-      <div>
-        <GatsbyImage image={getImage(node.childImageSharp)} alt="" />
-      </div>
+      <ImageListItem key={node.id}>
+        <img
+          src={`${node.publicURL}?w=248&fit=crop&auto=format`}
+          srcSet={`${node.publicURL}?w=248&fit=crop&auto=format&dpr=2 2x`}
+          loading="lazy"
+        />
+      </ImageListItem>
     )
   })
-  const hello = function(params) {
-    console.log('heeeloo');
+
+  const getMasonryColumn = () => {
+    const { innerWidth: width } = window
+    if (width < 700) {
+      return 3
+    } else if (width < 960) {
+      return 4
+    } else {
+      return 5
+    }
   }
+  const [masonryColumn, setMasonryColumn] = useState(getMasonryColumn())
+  useEffect(() => {
+    const onResize = () => {
+      setMasonryColumn(getMasonryColumn())
+    }
+    window.addEventListener("resize", onResize)
+    return () => window.removeEventListener("resize", onResize)
+  }, [])
 
   return (
     <Layout>
       <Seo title="Page two" />
-      <div className="gallery-height">
-        <StackGrid
-          columnWidth={200}
-          onLayout={hello}>
-          {GatsbyImages}
-        </StackGrid>
-      </div>
+      <ImageList variant="masonry" cols={masonryColumn} gap={8}>
+        {GatsbyImages}
+      </ImageList>
     </Layout>
   )
 }
@@ -44,15 +58,16 @@ export default ({ data, location }) => {
 export const query = graphql`
   query {
     about: allFile {
-    edges {
-      node {
-        id
-        absolutePath
-        childImageSharp {
-          gatsbyImageData
+      edges {
+        node {
+          id
+          absolutePath
+          publicURL
+          childImageSharp {
+            gatsbyImageData
+          }
         }
       }
     }
   }
-}
 `
