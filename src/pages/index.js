98cf4d101f8c3978a435ec535a5b6e4c9678c756
yus <ajtdmwgp42431@gmail.com>
2022-11-14 23:59:40 +0900

    表示修正

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,23 +5,11 @@ import { StaticImage } from "gatsby-plugin-image"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Img from "gatsby-image"
+import parse from "html-react-parser"
 
 export const query = graphql`
   query {
-    file(relativePath: {eq: "hero.jpg"}) {
-      childrenImageSharp {
-        fluid(maxWidth: 1600) {
-          aspectRatio
-          base64
-          originalImg
-          originalName
-          presentationWidth
-          presentationHeight
-          sizes
-        }
-      }
-    },
-    allMicrocmsBlog(limit: 3, sort: {fields: createdAt, order: DESC}) {
+    allMicrocmsBlog(limit: 3, sort: { fields: createdAt, order: DESC }) {
       nodes {
         id
         category
@@ -36,68 +24,41 @@ export const query = graphql`
     }
   }
 `
-/*
-export const blogs = graphql`
-  query {
-    microcmsBlog(blogId: {}) {
-      id
-      content
-      category
-      title
+
+export default ({ data }) => {
+  console.log(data)
+  console.log("blogs")
+  const newsQuery = data.allMicrocmsBlog.nodes
+
+  const blogs = data.allMicrocmsBlog.nodes.map(blog => {
+    let image = null
+    if (blog.samne) {
+      image = <img src={blog.samne.url} alt="" />
+    } else {
+      image = <StaticImage src="../images/noimage2.jpg" alt="no image" />
     }
-  }
-`
-*/
-
-export default ({data}) => {
-  console.log(data.allMicrocmsBlog.nodes)
-  console.log('blogs')
-  const newsQuery = data.allMicrocmsBlog.nodes;
-
-  const blogs = data.allMicrocmsBlog.nodes.map((blog) => {
-    const image = blog.samne ? blog.samne : {};
     console.log(image)
-    console.log('(image)')
+    console.log("(image)")
     return (
       <div className="detail" key={blog.id}>
         <h3>{blog.title}</h3>
-        <figure>
-          <img src={image.url} alt="" />
-        </figure>
+        <figure>{image}</figure>
         <p>{blog.subtitle}</p>
-        <p>{blog.content}</p>
+        <p>{parse(blog.content.substring(0, 140))}</p>
       </div>
     )
   })
 
-  /*
-  blogs.push(
-    <div className="detail">
-      <figure>
-        <img src="images/IMGP4909.JPG" alt="" />
-      </figure>
-      <h3>紅葉（本土寺）撮影会</h3>
-      <p>Hondoji Temple</p>
-      <p>紅葉を撮りに行きました。</p>
-    </div>
-  )
-  */
-
-  let news = [];
+  let news = []
   for (let i = 0; i < 3 && i < newsQuery.length; i++) {
-    news.push(
-      <div className="detail">
-      </div>
-    )
+    news.push(<div className="detail"></div>)
   }
-  
-              
+
   return (
     <div>
       <Layout>
         <section className="hero">
-          <figure>
-          </figure>
+          <figure></figure>
           <div className="catch">
             <h1>カメカフェ</h1>
             <p>東京を中心に活動する初心者向けカメラサークル</p>
@@ -106,18 +67,16 @@ export default ({data}) => {
         <section className="food">
           <div className="container">
             <h2 className="bar">お知らせ</h2>
-            <div className="details">
-              {news}
-            </div>
+            <div className="details">{news}</div>
           </div>
         </section>
 
         <section className="food">
           <div className="container">
-            <h2 className="bar">新着 <span></span></h2>
-            <div className="details">
-              {blogs}
-            </div>
+            <h2 className="bar">
+              新着 <span></span>
+            </h2>
+            <div className="details">{blogs}</div>
           </div>
         </section>
       </Layout>
