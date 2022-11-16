23db7f48abb1c275f5af8d140ea51b9e513f406b
yus <ajtdmwgp42431@gmail.com>
2022-11-17 00:13:35 +0900

    画像サイズ指定

--- src/components/gallery/modal.js
+++ src/components/gallery/modal.js
@@ -1,14 +1,20 @@
 import React from "react"
 
 export default ({ show, setShow, node }) => {
-  console.log(show, "cccccccc", node)
   const closeModal = () => {
     setShow(false)
   }
   if (show) {
     return (
       <div id="overlay" onClick={closeModal}>
-        <div id="content" onClick={e => e.stopPropagation()}>
+        <div
+          id="content"
+          style={{
+            width: node.photo.width < node.photo.height ? "40%" : "50%",
+            height: node.photo.width < node.photo.height ? "70%" : "50%",
+          }}
+          onClick={e => e.stopPropagation()}
+        >
           <div className="modal-header"></div>
           <img src={node.photo.url} />
           <button className="modal-close-btn" onClick={closeModal}>
