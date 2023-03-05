c18c04ac41395a60e9b125ccbe89a6b72cad189f
yus <ajtdmwgp42431@gmail.com>
2023-03-05 17:17:42 +0900

    buildエラーの対応

---
+++ src/pages/form_send.js
@@ -0,0 +1,11 @@
+import * as React from "react"
+import Layout from "../components/layout"
+
+const FormSentPage = () => {
+  return (
+    <Layout pageTitle="Form Sent Page">
+      <h2>フォームが送信されました</h2>
+    </Layout>
+  )
+}
+export default FormSentPage
