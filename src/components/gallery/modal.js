04e3d9b66ce5fcca13155a745d5fbca6035dd036
yus <ajtdmwgp42431@gmail.com>
2023-08-11 16:12:26 +0900

    ギャラリー更新

--- src/components/gallery/modal.js
+++ src/components/gallery/modal.js
@@ -1,81 +1,37 @@
 import React from "react"
 
-export default ({ show, setShow, node, photoIndex, slideNode, maxIndex }) => {
-  const closeModal = () => {
-    setShow(false)
-  }
-  const getTerminalType = () => {
-    let documentWindow = null
-    if (typeof window !== `undefined`) {
-      documentWindow = window
+import "../../styles/galley.modal.css"
+
+const Modal = ({ show, setShow, node, photoIndex, slideNode, maxIndex }) => {
+  const handleModalClick = e => {
+    const rect = e.currentTarget.getBoundingClientRect()
+    const halfWidth = rect.width / 2
+    if (e.clientX - rect.left < halfWidth) {
+      if (photoIndex > 0) {
+        slideNode(photoIndex - 1)
+      }
     } else {
-      return 4
-    }
-    const { innerWidth: width } = documentWindow
-    if (width < 700) {
-      return 3
-    } else if (width < 960) {
-      return 4
-    } else {
-      return 5
+      if (photoIndex < maxIndex - 1) {
+        slideNode(photoIndex + 1)
+      }
     }
   }
 
-  if (show) {
-    let width = null
-    if (getTerminalType() === 5) {
-      width = node.photo.width < node.photo.height ? "40%" : "50%"
-    } else {
-      width = "100%"
-    }
-    return (
-      <div id="overlay" onClick={closeModal}>
-        <div
-          id="content"
-          style={{
-            width: width,
-            height: node.photo.width < node.photo.height ? "70%" : "50%",
-            position: "relative",
-          }}
-          onClick={e => e.stopPropagation()}
-        >
-          <div className="modal-header"></div>
-          <img src={node.photo.url} />
-          {photoIndex > 1 && (
-            <div
-              style={{
-                width: "50%",
-                height: "90%",
-                opacity: 0,
-                position: "absolute",
-                top: 0,
-                left: 0,
-              }}
-              className="modal-before-btn"
-              onClick={() => slideNode(--photoIndex)}
-            ></div>
-          )}
-          {photoIndex < maxIndex - 1 && (
-            <div
-              style={{
-                width: "50%",
-                height: "90%",
-                opacity: 0,
-                position: "absolute",
-                top: 0,
-                right: 0,
-              }}
-              className="modal-next-btn"
-              onClick={() => slideNode(++photoIndex)}
-            ></div>
-          )}
-          <button className="modal-close-btn" onClick={closeModal}>
-            とじる
-          </button>
-        </div>
+  return show ? (
+    <div className="modalOverlay" onClick={() => setShow(false)}>
+      <div
+        className="modalContent"
+        onClick={e => e.stopPropagation()}
+        onPointerDown={handleModalClick}
+      >
+        <img
+          src={node.photo.url}
+          alt={node.title}
+          style={{ background: "grey" }} // 余白をグレー色にするためのスタイル
+        />
       </div>
-    )
-  } else {
-    return null
-  }
+    </div>
+  ) : null
 }
+
+export default Modal
--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,122 +1,85 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
 
 import Layout from "../components/layout"
 import Modal from "../components/gallery/modal"
 import Seo from "../components/seo"
 import { graphql } from "gatsby"
 
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { ImageList, ImageListItem, ImageListItemBar } from "@mui/material"
-
+import { ImageList, ImageListItem } from "@mui/material"
 import "../styles/galley.module.css"
 
 export default ({ data, location }) => {
   const dispPerLength = 25
-  const [imageStart, setImageStart] = useState(0)
-  const [show, setShow] = useState(false)
-  const [modalPhoto, setModalPhoto] = useState(false)
-  const [photoIndex, setPhotoIndex] = useState(0)
   const [currentPhotoMaxLength, setCurrentPhotoMaxLength] =
     useState(dispPerLength)
+  const [show, setShow] = useState(false)
+  const [modalPhoto, setModalPhoto] = useState(null)
+  const [photoIndex, setPhotoIndex] = useState(0)
+  const loadMoreRef = useRef()
 
   const showModal = (node, i) => {
     setShow(true)
     setPhotoIndex(i)
     setModalPhoto(node)
   }
-  const slideNode = index => {
-    setPhotoIndex(index)
-    setModalPhoto(data.photos.nodes[index])
-  }
 
-  const GatsbyImages = data.photos.nodes
-    .slice(0, currentPhotoMaxLength)
-    .sort((first, second) => {
-      if (first.shoot_date > second.shoot_date) {
-        return -1
-      } else if (first.shoot_date < second.shoot_date) {
-        return 1
-      } else {
-        return 0
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting) {
+        setCurrentPhotoMaxLength(prevLength => prevLength + dispPerLength)
       }
     })
-    .map((node, i) => {
-      return (
-        <ImageListItem className="photo-gallery" key={node.id}>
-          <a href="#" onClick={() => showModal(node, i)}>
-            <img
-              className="photo-gallery-image"
-              src={`${node.photo.url}?w=248&fit=crop&auto=format`}
-              srcSet={`${node.photo.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
-              loading="lazy"
-            />
-          </a>
-        </ImageListItem>
-      )
-    })
 
-  const getMasonryColumn = () => {
-    let documentWindow = null
-    if (typeof window !== `undefined`) {
-      documentWindow = window
-    } else {
-      return 4
+    if (loadMoreRef.current) {
+      observer.observe(loadMoreRef.current)
     }
-    const { innerWidth: width } = documentWindow
-    if (width < 700) {
-      return 3
-    } else if (width < 960) {
-      return 4
-    } else {
-      return 5
-    }
-  }
-  const [masonryColumn, setMasonryColumn] = useState(getMasonryColumn())
-  useEffect(() => {
-    const onResize = () => {
-      setMasonryColumn(getMasonryColumn())
-    }
-    window.addEventListener("resize", onResize)
-    return () => window.removeEventListener("resize", onResize)
-  }, [])
 
-  const changeMaxLength = () => {
-    const innerHeight = window.innerHeight
-    const currentHeight = window.scrollY
-    const perHeight = 300
-    const photoMaxLenth = Math.ceil(currentHeight / perHeight) * dispPerLength
-    if (currentPhotoMaxLength < photoMaxLenth) {
-      setCurrentPhotoMaxLength(photoMaxLenth)
+    return () => {
+      if (loadMoreRef.current) {
+        observer.unobserve(loadMoreRef.current)
+      }
     }
-  }
-
-  useEffect(() => {
-    window.addEventListener("scroll", changeMaxLength)
-    return () => window.removeEventListener("scroll", changeMaxLength)
   }, [])
 
   return (
     <Layout>
       <Seo title="ギャラリー" />
-      <ImageList
-        variant="masonry"
-        id="photo-galleries"
-        cols={masonryColumn}
-        gap={8}
-      >
-        {GatsbyImages}
+      <ImageList variant="masonry" id="photo-galleries" cols={4} gap={8}>
+        {data.photos.nodes.slice(0, currentPhotoMaxLength).map((node, i) => (
+          <ImageListItem className="photo-gallery" key={node.id}>
+            <a
+              href="#"
+              onClick={e => {
+                e.preventDefault()
+                showModal(node, i)
+              }}
+            >
+              <img
+                className="photo-gallery-image"
+                src={`${node.photo.url}?w=248&fit=crop&auto=format`}
+                srcSet={`${node.photo.url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                loading="lazy"
+              />
+            </a>
+          </ImageListItem>
+        ))}
+        {currentPhotoMaxLength < data.photos.nodes.length && (
+          <div ref={loadMoreRef} style={{ height: "20px", margin: "10px" }}>
+            Loading more images...
+          </div>
+        )}
       </ImageList>
       <Modal
         show={show}
         setShow={setShow}
         node={modalPhoto}
         photoIndex={photoIndex}
-        slideNode={slideNode}
+        slideNode={index => {
+          setPhotoIndex(index)
+          setModalPhoto(data.photos.nodes[index])
+        }}
         maxIndex={data.photos.nodes.length}
       />
-      <br />
-      <br />
-      <br />
     </Layout>
   )
 }
